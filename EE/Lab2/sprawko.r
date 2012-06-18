8f22a976d0b1cb283f052123f794858fab107ead
Łukasz Dubiel <bambucha14@gmail.com>
2012-06-18 04:59:45 +0200

    Dodanie i praca nad sprawozdaniem.

--- EE/Lab2/sprawko.r
+++ EE/Lab2/sprawko.r
@@ -15,21 +15,78 @@ plot(dane)
 abline(l)
 dev.off()
 
-d <- subset(dane,I > 0)
+d <- dane
 
-d$U = d$U / Ut 
-d$I = log10(d$I)
+d$U <- d$U / Ut 
+d$I <- d$I + abs(min(min(d$I),0))
+d <- subset(d, I > 0)
+d$I <- log10(d$I)
+
+x <- subset(d,U > 16 & U < 20)
+zero <- lm(rev(x))
+print(zero)
+Izero <- exp(zero$coefficients[1])
+print(Izero)
+Nzero <- 1/zero$coefficients[2];
+print(Nzero)
+
+gr <- subset(d, U > 20 & U < 27)
+GRl <- lm(rev(gr))
+print(GRl)
+Izero <- exp(GRl$coefficients[1])
+print(Izero)
+Nzero <- 1/(2*GRl$coefficients[2])
+print(Nzero)
 
 
+png(lout)
+plot(d,ylim=c(-10,-0))
+abline(zero)
+abline(GRl)
+dev.off()
+#######################################################################
+#Prostownicza koniec tutaj
+#######################################################################
+typ <- "germanowa"
+plik <- paste(typ,".txt",sep="")
+nout <- paste("out/",typ,"-normal.png",sep="")
+lout <- paste("out/",typ,"-log.png",sep="")
+dane <- read.csv2(plik,dec=",")
+
+png(nout)
+l <- lm(tail(rev(dane),n=400))
+Vzero <- polyroot(as.vector(l$coefficients))
+print(Vzero)
+plot(dane)
+abline(l)
+dev.off()
+
+d <- dane
+
+d$U <- d$U / Ut 
+d$I <- d$I + abs(min(min(d$I),0))
+d <- subset(d, I > 0)
+d$I <- log10(d$I)
+
+x <- subset(d,U > 16 & U < 20)
+zero <- lm(rev(x))
+print(zero)
+Izero <- exp(zero$coefficients[1])
+print(Izero)
+Nzero <- 1/zero$coefficients[2];
+print(Nzero)
+
+gr <- subset(d, U > 20 & U < 27)
+GRl <- lm(rev(gr))
+print(GRl)
+Izero <- exp(GRl$coefficients[1])
+print(Izero)
+Nzero <- 1/(2*GRl$coefficients[2])
+print(Nzero)
 
 
-
-
-
-
-
-
-
-
-
-
+png(lout)
+plot(d,ylim=c(-10,-0))
+abline(zero)
+abline(GRl)
+dev.off()
